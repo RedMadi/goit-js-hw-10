7de97ab6ec6a4face11f6c820cb8763a39dba332
RedMadi <59694296+RedMadi@users.noreply.github.com>
2023-08-10 06:36:53 +0300

    files

---
+++ src/cat-api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_kbszEMZZ4RuDhF4Ji2l3qlxYjQmzA5erm1YDV8rL3yPBqz5THUCcujcH4UKXkSa4';
+
+const pLoader = document.querySelector('.loader');
+const catInfo = document.querySelector('.cat-info');
+
+export const fetchBreeds = () => {
+  Notiflix.Loading.custom(pLoader.textContent, {
+    customSvgUrl:
+      'https://notiflix.github.io/content/media/loading/notiflix-loading-nx-light.svg',
+  });
+  return axios.get('https://api.thecatapi.com/v1/breeds');
+};
+
+export const fetchCatByBreed = breedId => {
+  return axios.get(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  );
+};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+// import axios from 'axios';
+
+// axios.defaults.headers.common['x-api-key'] =
+//   'live_kbszEMZZ4RuDhF4Ji2l3qlxYjQmzA5erm1YDV8rL3yPBqz5THUCcujcH4UKXkSa4';
